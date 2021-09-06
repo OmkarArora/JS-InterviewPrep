579ff320c3e5b557de1c845f8df918c6cb1fd510
Omkar Arora <aroraomkar@gmail.com>
2021-09-06 12:36:09 +0530

    fix: bind polyfill

--- polyfills/bind.js
+++ polyfills/bind.js
@@ -1,5 +1,16 @@
-function bind(fn, thisArg){
-	return function(){ 
-		return fn.apply(thisArg, args);
-	}
+function myBind(fn, context) {
+  return function () {
+    return fn.apply(context, arguments);
+  };
 }
+
+let obj = {
+  type: "ball",
+  color: "blue",
+};
+
+function describe(extraArg) {
+  console.log(this.type, this.color, extraArg);
+}
+
+myBind(describe, obj)("apple");
